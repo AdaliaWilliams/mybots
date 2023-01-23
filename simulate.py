c54508139a5316374e75e48319b90da48858f536
AdaliaWilliams <Adalia.Williams@uvm.edu>
2023-01-23 15:43:57 -0500

    added time.sleep command

--- simulate.py
+++ simulate.py
@@ -1,6 +1,13 @@
 import pybullet as p
+import time
+
 def simulate():
     physicsClient = p.connect(p.GUI)
+    for i in range(1000):
+        print(i)
+        time.sleep(3)
+        p.stepSimulation()
+        
     p.disconnect()
 
 simulate()
