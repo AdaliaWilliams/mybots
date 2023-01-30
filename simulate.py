4dbf02f07171c43fe902226e888caaf171b8d67f
AdaliaWilliams <Adalia.Williams@uvm.edu>
2023-01-30 15:54:13 -0500

    finished code for many links

--- simulate.py
+++ simulate.py
@@ -1,9 +1,13 @@
 import pybullet as p
 import time
+import pybullet_data
 
 def simulate():
     physicsClient = p.connect(p.GUI)
-    p.loadSDF("box.sdf")
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    planeId = p.loadURDF("plane.urdf")
+    p.setGravity(0,0,-9.8)
+    p.loadSDF("boxes.sdf")
     for i in range(1000):
         print(i)
         time.sleep(1/60)
