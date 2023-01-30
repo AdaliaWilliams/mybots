cb5d514975fdaeab1f1ede4f906d94d644da6a6d
AdaliaWilliams <Adalia.Williams@uvm.edu>
2023-01-30 16:10:59 -0500

    completed creating a robot and a world

--- generate.py
+++ generate.py
@@ -1,8 +1,15 @@
 import pyrosim.pyrosim as pyrosim
 
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")  
+    pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.Start_SDF("world.sdf")
+   
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-     
-pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -6,6 +6,7 @@ def simulate():
     physicsClient = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     planeId = p.loadURDF("plane.urdf")
+    robotId = p.loadURDF("body.urdf")
     p.setGravity(0,0,-9.8)
     p.loadSDF("world.sdf")
     for i in range(1000):
@@ -14,5 +15,8 @@ def simulate():
         p.stepSimulation()
         
     p.disconnect()
+
+
+
 
 simulate()
