365d2f1817fa68cbff1600de1cb0f787601cbe85
AdaliaWilliams <Adalia.Williams@uvm.edu>
2023-02-06 13:47:49 -0500

    added sensors, but does not seem to be working

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import time
 import pybullet_data
 
@@ -9,8 +10,12 @@ def simulate():
     robotId = p.loadURDF("body.urdf")
     p.setGravity(0,0,-9.8)
     p.loadSDF("world.sdf")
+    pyrosim.Prepare_To_Simulate(robotId)
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    
     for i in range(1000):
-        print(i)
+        #print(i)
+        print(backLegTouch)
         time.sleep(1/60)
         p.stepSimulation()
         
