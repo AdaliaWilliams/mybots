c02c5fab24f10ae799b49125f6fb4733c2345d4a
AdaliaWilliams <Adalia.Williams@uvm.edu>
2023-02-10 11:34:53 -0500

    set both joints with motors

--- analyze.py
+++ analyze.py
@@ -1,1 +1,13 @@
 import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.load('data/backLegSensorValues.npy', mmap_mode='r')
+frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy', mmap_mode='r')
+print(backLegSensorValues)
+print(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg', linewidth = 5)
+matplotlib.pyplot.plot(frontLegSensorValues, label = 'Front Leg')
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -13,18 +13,33 @@ def simulate():
     p.setGravity(0,0,-9.8)
     p.loadSDF("world.sdf")
     backLegSensorValues = numpy.zeros(100)
+    frontLegSensorValues = numpy.zeros(100)
     pyrosim.Prepare_To_Simulate(robotId) 
     
     for i in range(100):
         #print(i)
         backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+        frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
         time.sleep(1/60)
+        pyrosim.Set_Motor_For_Joint(
+            bodyIndex = robotId,
+            jointName = b'Torso_BackLeg',
+            controlMode = p.POSITION_CONTROL,
+            targetPosition = (3.14/4.0),
+            maxForce = 500)
+        pyrosim.Set_Motor_For_Joint(
+            bodyIndex = robotId,
+            jointName = b'Torso_FrontLeg',
+            controlMode = p.POSITION_CONTROL,
+            targetPosition = (-3.14/4.0),
+            maxForce = 500)
         p.stepSimulation()
         
     
     p.disconnect()
-    print(backLegSensorValues)
-    numpy.save('data/saveData.npy', backLegSensorValues)
+    #print(backLegSensorValues)
+    numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
+    numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
 
 
 
